6109b652282ac50920184e28acee29109b1bba3f
KirillSidorov <kir_sidorov@mail.ru>
2024-07-02 17:44:25 +0300

    graceful shutdown

--- db/db.go
+++ db/db.go
@@ -13,12 +13,12 @@ var db *sql.DB
 func Init() {
 	dbUser, found := os.LookupEnv("db_username")
 	if !found {
-		log.Panic("environment variable db_username not found in .env")
+		log.Fatal("environment variable db_username not found in .env")
 	}
 
 	dbPassword, found := os.LookupEnv("db_password")
 	if !found {
-		log.Panic("environment variable db_password not found in .env")
+		log.Fatal("environment variable db_password not found in .env")
 	}
 
 	connStr := fmt.Sprintf("user=%s password=%s dbname=webchat sslmode=disable", dbUser, dbPassword)
@@ -26,6 +26,14 @@ func Init() {
 	var err error
 	db, err = sql.Open("postgres", connStr)
 	if err != nil {
-		log.Panic(err)
+		log.Fatal(err)
+	}
+}
+
+func Close() {
+	log.Println("DB close connection")
+	err := db.Close()
+	if err != nil {
+		log.Println(err)
 	}
 }
--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"context"
 	"encoding/gob"
+	"errors"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"strings"
+	"syscall"
+	"time"
 	"webchat/chat"
 	"webchat/db"
 	"webchat/handler"
@@ -13,6 +18,11 @@ import (
 
 	"github.com/gorilla/sessions"
 	"github.com/joho/godotenv"
+)
+
+const (
+	listenAddr      = "127.0.0.1:8080"
+	shutdownTimeout = 3 * time.Second
 )
 
 var (
@@ -23,32 +33,70 @@ var (
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Panic(".env file not found")
+		log.Fatal(".env file not found")
 	}
 
 	db.Init()
 
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	if err := runWebServer(ctx); err != nil {
+		log.Println(err)
+	}
+
+	db.Close()
+}
+
+func runWebServer(ctx context.Context) error {
+
 	secret, found := os.LookupEnv("cookie_store_secret")
 	if !found {
-		log.Panic("environment variable cookie_store_secret not found in .env")
+		return errors.New("environment variable cookie_store_secret not found in .env")
 	}
 	store = sessions.NewCookieStore([]byte(secret))
 	store.MaxAge(86400)
 
 	gob.Register(users.User{})
 
-	server := http.NewServeMux()
-	server.HandleFunc("/", baseHandler)
-	server.HandleFunc("/favicon.ico", faviconHandler)
-
 	hub = chat.NewHub()
 	go hub.Run()
 
-	log.Println("Server Started: http://localhost:8080")
-	err = http.ListenAndServe("localhost:8080", server)
-	if err != nil {
-		log.Fatal(err)
+	var mux = http.NewServeMux()
+	var srv = &http.Server{
+		Addr:    listenAddr,
+		Handler: mux,
 	}
+
+	mux.HandleFunc("/", baseHandler)
+	mux.HandleFunc("/favicon.ico", faviconHandler)
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			db.Close()
+			log.Fatalf("Server Listen and Serve: %v", err)
+		}
+	}()
+	log.Printf("Server Started: %s", listenAddr)
+	
+	<-ctx.Done()
+	log.Println("Shutting down server gracefully")
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), shutdownTimeout)
+	defer cancel()
+
+	if err := srv.Shutdown(shutdownCtx); err != nil {
+		return err
+	}
+
+	<-shutdownCtx.Done()
+
+	err := ctx.Err()
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func baseHandler(response http.ResponseWriter, request *http.Request) {
