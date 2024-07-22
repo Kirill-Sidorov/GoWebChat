d7bdde432429457b6e38eaf21b48a9954a22cada
KirillSidorov <kir_sidorov@mail.ru>
2024-07-22 22:19:42 +0300

    registration form and slim docker image

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,12 @@
-FROM golang:1.22.5-alpine3.19
-# Set the Current Working Directory inside the container
-WORKDIR $GOPATH/src/github.com/Kirill-Sidorov/GoWebChat
-
-# Copy everything from the current directory to the PWD (Present Working Directory) inside the container
+FROM golang:1.22.5-alpine3.19 AS builder
+WORKDIR /
 COPY . .
+RUN go mod download
+RUN go build -o .
 
-# Download all the dependencies
-RUN go get -d -v ./...
-
-# Install the package
-RUN go install -v ./...
-
-# This container exposes port 8080 to the outside world
+FROM gcr.io/distroless/base-debian10
+WORKDIR /
+COPY --from=builder /resources /resources
+COPY --from=builder .env webchat /
 EXPOSE 8080
-
-# Run the executable
-CMD ["webchat"]
+ENTRYPOINT ["/webchat"]
--- handler/registration.go
+++ handler/registration.go
@@ -7,12 +7,47 @@ import (
 	"github.com/gorilla/sessions"
 )
 
+type showRegistrationPage struct {
+	NameInput			 string
+	LoginInput 			 string
+	ErrorRegisterMessage string
+}
+
 func ShowRegistrationPage(response http.ResponseWriter, request *http.Request, session *sessions.Session) {
 	
-	var data struct{}
-	err := registrationPageHtml.Execute(response, data)
+	nameInput, hasName := session.Values["NameInput"].(string)
+	loginInput, hasLogin := session.Values["LoginInput"].(string)
+	errorRegisterMessage, hasError := session.Values["ErrorRegisterMessage"].(string)
 
+	if !hasName {
+		nameInput = ""
+	}
+	if !hasLogin {
+		loginInput = ""
+	}
+	if !hasError {
+		errorRegisterMessage = ""
+	}
+
+	delete(session.Values, "NameInput")
+	delete(session.Values, "LoginInput")
+	delete(session.Values, "ErrorRegisterMessage")
+
+	err := session.Save(request, response)
 	if err != nil {
 		log.Println(err)
 	}
+
+	err = registrationPageHtml.Execute(response, &showRegistrationPage{
+		NameInput: nameInput,
+		LoginInput: loginInput, 
+		ErrorRegisterMessage: errorRegisterMessage,
+	})
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func Register(response http.ResponseWriter, request *http.Request, session *sessions.Session) {
+
 }
